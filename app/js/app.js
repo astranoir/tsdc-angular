c09f999750bb8c30e405933abb4b69a43346b10d
William Scott <wscott@amplify.com>
2015-02-20 13:09:36 -0500

    Start Todo application.

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,21 @@
 var app = angular.module('tsdc', []);
 
 app.controller('AppCtrl', function($scope) {
-    $scope.name = 'From the controller!';
+    $scope.todos = [{
+        text:'Finish Todo App',
+        done: false
+    }];
+
+    $scope.addTodo = function(newTodo) {
+        if(!newTodo) {
+            return;
+        }
+
+        $scope.todos.push({
+            text: newTodo,
+            done: false
+        });
+
+        $scope.newTodo = '';
+    }
 });
