05e11a25fbc755c3d47cd228d86981915ac30322
William Scott <wscott@amplify.com>
2015-02-20 14:15:45 -0500

    Use route to select todos by status.

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,33 @@
 var app = angular.module('tsdc', ['ngRoute']);
 
 app.config(function($routeProvider){
+    var routeConfig = {
+        controller: 'AppCtrl',
+        templateUrl: 'todo.html',
+        reloadOnSearch: false
+    };
     $routeProvider
-        .when('/', {
-            controller: 'AppCtrl',
-            templateUrl: 'todo.html'
-        })
+        .when('/', routeConfig)
+        .when('/:status', routeConfig)
         .otherwise({
             redirectTo: '/'
         })
 });
 
-app.controller('AppCtrl', function($scope) {
+app.controller('AppCtrl', function($scope, $routeParams) {
     $scope.todos = [{
         text:'Finish Todo App',
         done: false
     }];
+
+    $scope.$on('$routeChangeSuccess', function() {
+        var status = $routeParams.status;
+        if(angular.isUndefined(status)) {
+            $scope.statusFilter = {};
+        } else {
+            $scope.statusFilter = {done: (status === 'done')};
+        }
+    });
 
     $scope.clearDoneTodos = function () {
         $scope.todos = $scope.todos.filter(function (todo) {
