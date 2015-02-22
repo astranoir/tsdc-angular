22193f404bd34d9c8b28a685fb4d81e3936b7566
astranoir <beebube@gmail.com>
2015-02-22 14:16:04 -0500

    Review tutorial and add enter functionality
    Go through tutorial;
    added functionality:
    in addition to clicking the button to add a to do item, hitting the
    Enter key will also add a to do item

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,11 @@
 var app = angular.module('tsdc', ['ngRoute']);
+    //angular.module is a global place for registering and retrieving Angular modules
+    //All modules that should be available to an app must be registered this way
 
-app.config(function($routeProvider){
-    var routeConfig = {
+app.config(function($routeProvider)
+{
+    var routeConfig =
+    {
         controller: 'AppCtrl',
         templateUrl: 'todo.html',
         reloadOnSearch: false
@@ -9,55 +13,97 @@ app.config(function($routeProvider){
     $routeProvider
         .when('/', routeConfig)
         .when('/:status', routeConfig)
-        .otherwise({
+        .otherwise(
+        {
             redirectTo: '/'
         })
 });
 
-app.factory('todoStore', function() {
+app.factory('todoStore', function()
+{
     return {
         todos: []
     }
 });
 
-app.controller('AppCtrl', function($scope, $routeParams, todoStore) {
+app.controller('AppCtrl', function($scope, $routeParams, todoStore)
+    //In Angular, a Controller is a JavaScript constructor function
+    //used to augment the Angular scope
+    //A new child scope will be available as an injectable parameter
+    //to the  constructor function of the Controller as $scope
+{
     $scope.todos = todoStore.todos;
 
-    $scope.$on('$routeChangeSuccess', function() {
+    $scope.$on('$routeChangeSuccess', function()
+    {
         var status = $routeParams.status;
-        if(angular.isUndefined(status)) {
+        if(angular.isUndefined(status))
+        {
             $scope.statusFilter = {};
-        } else {
+        }
+        else
+        {
             $scope.statusFilter = {done: (status === 'done')};
         }
     });
 
-    $scope.clearDoneTodos = function () {
-        $scope.todos = $scope.todos.filter(function (todo) {
+    $scope.clearDoneToDos = function()
+    {
+        $scope.todos = $scope.todos.filter(function (todo)
+        {
             return !todo.done;
         })
     };
 
-    $scope.addTodo = function(newTodo) {
-        if(!newTodo) {
+    $scope.addToDo = function(newToDo)
+    {
+        if(!newToDo)
+        {
             return;
         }
 
-        $scope.todos.push({
-            text: newTodo,
+        $scope.todos.push
+        ({
+            text: newToDo,
             done: false
         });
 
-        $scope.newTodo = '';
+        $scope.newToDo = '';
     }
 });
 
-app.directive('todoItem', function() {
-    return {
-        restict: 'E',
-        scope: {
+app.directive('ngEnter', function()
+    //app.directive lets you add custom directives in Angular
+    //this custom directive allows passing a function on an Enter keypress
+
+{
+    return function (scope, element, attrs) //note to self: attrs is attribute set
+    {
+        element.bind("keydown keypress", function (event)
+        {
+            if(event.which === 13)  //13 is the Enter key
+            {
+                scope.$apply(function()
+                {
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+app.directive('todoItem', function()
+{
+    return{
+        restrict: 'E',  //E is for element; A is for Attribute
+        scope:
+        {
             data: '='
         },
         template: '<div>\n    <label class="todo-{{ (!data.done) ? \'active\' : \'done\'}}">\n        <input type="checkbox" ng-model="data.done"/>\n        {{data.text}}\n    </label>\n</div>'
+        //Template-expanding directive simplifies your template
+        //Best practice: Break your template out into its own HTML file and load it with the templateUrl option
+        //unless your template is very small
     }
 });
